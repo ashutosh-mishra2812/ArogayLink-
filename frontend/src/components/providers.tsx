99de6950ceee03f2e8c33afed271d5d38d788b56
ashutosh_mishra2812 <2k23.csaiml2313890@gmail.com>
2025-10-10 04:15:21 +0530

    onboarding and dashboard pages for doctor and patient roles.

---
+++ frontend/src/components/providers.tsx
@@ -0,0 +1,13 @@
+import { useAuthStore } from "@/store/authStore";
+import { useEffect } from "react";
+
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const { fetchProfile, token } = useAuthStore();
+  useEffect(() => {
+    if (token) {
+      fetchProfile();
+    }
+  }, [token, fetchProfile]);
+        return <>{children}</>; 
+}
